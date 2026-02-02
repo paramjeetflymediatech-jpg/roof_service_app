8c6af0ad95050de56870f87deffb91be9a3f7254
Param <paramjeet.flymediatech@gmail.com>
2026-02-02 18:40:08 +0530

    add onboarding screen

--- RoofServiceApp/App.jsx
+++ RoofServiceApp/App.jsx
@@ -103,6 +103,7 @@ export const useAuth = () => {
 };
 
 // Screens
+import OnboardingScreen from './src/screens/OnboardingScreen';
 import LoginScreen from './src/screens/LoginScreen';
 import ClientHomeScreen from './src/screens/ClientHomeScreen';
 import ClientQuoteScreen from './src/screens/ClientQuoteScreen';
@@ -120,7 +121,11 @@ const App = () => {
   return (
     <AuthProvider>
       <NavigationContainer>
-        <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Navigator
+          screenOptions={{ headerShown: false }}
+          initialRouteName="Onboarding"
+        >
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="ClientHome" component={ClientHomeScreen} />
           <Stack.Screen name="ClientQuote" component={ClientQuoteScreen} />
