9a84fe417bed168e76b02aa63e5a551add6a6961
Param <paramjeet.flymediatech@gmail.com>
2026-02-05 13:05:43 +0530

    d

--- backend/src/app.js
+++ backend/src/app.js
@@ -31,7 +31,7 @@ app.use(
     origin: allowedOrigins,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    // allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
