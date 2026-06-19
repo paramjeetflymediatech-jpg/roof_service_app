5b97395f6918cb59e82b4aa7d598dff25a1cf2f5
Paramjit singh <paramjeet.flymediatech@gmail.com>
2026-06-19 11:01:09 +0530

    ademail

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -24,7 +24,7 @@ transporter.verify((error, success) => {
 const sendLeadNotification = async (leadData) => {
   const mailOptions = {
     from: process.env.EMAIL_FROM,
-    to: "mainstreetroofing604@gmail.com,anujguptaflymedia@gmail.com,", // Send to your email
+    to: "mainstreetroofing604@gmail.com,anujguptaflymedia@gmail.com,paramjeet.flymediatech@gmail.com,paramjeet.flymediatech@gmail.com", // Send to your email
     subject: `New Lead: ${leadData.name}`,
     html: `
             <div style="font-family: Arial, sans-serif; max-width: 600px; color:black;margin: 0 auto;">
